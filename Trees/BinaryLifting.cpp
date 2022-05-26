cc85cb9de0c0b6fd5e561e7923c6d0854aba26c8
Fossil <Arixcrest@gmail.com>
2022-05-26 06:32:06 +0530

    Added Binary Lifting to answer LCA Queries

---
+++ Trees/BinaryLifting.cpp
@@ -0,0 +1,41 @@
+vector<vector<int>> graph;
+struct binlift{
+    int n,l;
+    int timer = 0;
+    vector<int> tin,tout;
+    vector<vector<int>> up;
+    binlift(int n){
+        tin.resize(n+1);
+        tout.resize(n+1);
+        l = ceil(log2(n));
+        up.assign(n+1,vector<int>(l+1));
+    }
+    void dfs(int cur,int par){
+        up[cur][0] = par;
+        tin[cur] = timer++;
+        for(int i = 1;i<=l;++i){
+            up[cur][i] = up[up[cur][i-1]][i-1];
+        }
+        for(auto x : graph[cur]){
+            if(x!=par){
+                dfs(x,cur);
+            }
+        }
+        tout[cur] = timer-1;
+    }
+
+    bool ancestor(int u,int v){
+        return (tin[u]<=tin[v] && tout[u]>=tout[v]);
+    }
+    //ones_index
+    int lca(int u,int v){
+        if(ancestor(u,v)) return u;
+        if(ancestor(v,u)) return v;
+        for(int i = l;i>=0;--i){
+            if(!ancestor(up[u][i],v)){
+                u = up[u][i];
+            }
+        }
+        return up[u][0];
+    }
+};
